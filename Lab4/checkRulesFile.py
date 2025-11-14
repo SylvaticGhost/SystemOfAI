0811696a55de53cb1f48806df44a1c7331799f30
Kostia <143171907+SylvaticGhost@users.noreply.github.com>
2025-11-14 23:09:23 +0200

    w4

--- Lab4/checkRulesFile.py
+++ Lab4/checkRulesFile.py
@@ -1,8 +1,29 @@
 ﻿import numpy as np
 
-data = np.load('sarsa-weights-berzerk-default-10k.npz')
+data = np.load('sarsa-weights-berzerk.npz')
 w = data['w']
 print(f'Weights shape: {w.shape}')
+print(w)
 
 print(f'Example weights for state 0: {w[0]}')
-print(f'Non-zero weights count: {np.count_nonzero(w)}')
+non_zero_count = np.count_nonzero(w)
+print(f'Non-zero count: {non_zero_count}, percentage: {non_zero_count / w.size * 100:.2f}%')
+
+min_val = np.min(w)
+max_val = np.max(w)
+mean_val = np.mean(w)
+std_val = np.std(w)
+median_val = np.median(w)
+q1_val = np.percentile(w, 25)
+q3_val = np.percentile(w, 75)
+iqr_val = q3_val - q1_val
+
+lower_normal_bound = median_val - 1.5 * iqr_val
+upper_normal_bound = median_val + 1.5 * iqr_val
+outliers = w[(w < lower_normal_bound) | (w > upper_normal_bound)]
+outlier_count = outliers.size
+
+print(f'Min: {min_val}, Max: {max_val}')
+print(f'Mean: {mean_val}, Std: {std_val}')
+print(f'Median: {median_val}, Q1: {q1_val}, Q3: {q3_val}, IQR: {iqr_val}')
+print(f'Outlier count: {outlier_count}, Outlier percentage: {outlier_count / w.size * 100:.2f}%')
---
+++ Lab4/w_normalize.py
@@ -0,0 +1,22 @@
+﻿import numpy as np
+
+w = np.array([
+    6.083236, 5.737967, 16.35171, 0.51813793, 1.2349157, 1.8856012,
+    8.547919, 8.85066, 8.397603, 8.921656, 8.118739, 8.5821495,
+    8.706381, 8.237435, 9.013649, 8.400361, 9.262568, 9.005248,
+    8.488785, 10.227346, 8.819691, 36.677753, 8.743167, 8.638772
+])
+
+avg = np.mean(w)
+std = np.std(w)
+median = np.median(w)
+iqr = np.percentile(w, 75) - np.percentile(w, 25)
+print(f'Average: {avg}, Standard Deviation: {std}, Median: {median}, IQR: {iqr}')
+
+lower_bound = avg - iqr*1
+upper_bound = avg + iqr*1
+
+print(f'Lower bound: {lower_bound}, Upper bound: {upper_bound}')
+
+w_cliped = np.clip(w, lower_bound, upper_bound)
+print('Normalized weights:', w_cliped)
